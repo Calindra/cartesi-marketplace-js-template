05bca6e5f9596aa9a822d3f9c5e1782426127680
Diego Guimaraes <diego.sena.guimaraes@gmail.com>
2024-09-02 16:01:41 -0300

    adding app start app

---
+++ app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Instala as dependências
+npm install
+
+# Inicia o comando nonodo e monitora a saída
+nonodo | {
+  while IFS= read -r line; do
+    echo "$line"  # Exibe a saída no console
+    if [[ "$line" == *"nonodo: ready"* ]]; then
+      # Quando a mensagem "nonodo: ready" for encontrada, saia do loop
+      break
+    fi
+  done
+
+  # Após o nonodo estar pronto, execute o npm start
+  npm start
+}
